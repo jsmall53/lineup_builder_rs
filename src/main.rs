eabd97ad7004cc5165889623454f3f62fb37cd2e
Jordan Small <jordansmall@WillieHillsMBP.fios-router.home>
2019-08-03 16:40:50 -0700

    adding support for basic args, not very robust

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,22 @@ extern crate clap;
 
 use std::error::Error;
 use std::process;
-use clap::{ App, Arg };
+use clap::{ App, Arg, ArgMatches };
 use builder::builder::{ Builder, Lineup };
 
-fn run(file_path: &str) -> Result<Vec<Lineup>, &'static str> {
+fn run(matches: ArgMatches) -> Result<Vec<Lineup>, &'static str> {
+    let input_file = matches.value_of("INPUT_FILE").unwrap(); // this is a required parameter
+    let provider = matches.value_of("provider").unwrap();
+    let contest_type = matches.value_of("contest-type").unwrap();
+    let sport = matches.value_of("sport").unwrap();
+
     let builder = Builder::new("./resources/game_templates/");
-    let result = builder.provider("draft_kings")
-                        .sport("nfl")
-                        .contest("classic")
-                        .slate(file_path)
+    let result = builder.provider(provider)
+                        .sport(sport)
+                        .contest(contest_type)
+                        .slate(input_file)
                         .build().expect("optimizer build step failed")
                         .optimize();
-
     result
 }
 
@@ -31,17 +35,26 @@ fn main() {
                         .author("Jordan Small")
                         .about("Optimizes DFS lineups")
                         .arg(Arg::with_name("INPUT_FILE")
-                                .help("Sets the input file to use")
-                                .required(true)
-                                .index(1))
+                            .help("Sets the input file to use")
+                            .required(true)
+                            .index(1))
                         .arg(Arg::with_name("provider")
-                                .short("p")
-                                .help("set the data provider"))
+                            .short("p")
+                            .default_value("draft_kings")
+                            .help("set the data provider"))
+                        .arg(Arg::with_name("contest-type")
+                            .short("c")
+                            .default_value("classic")
+                            .help("the type of contest the lineups are for"))
+                        .arg(Arg::with_name("sport")
+                            .short("s")
+                            .default_value("nfl")
+                            .help("sets the sport type"))
                         .get_matches();
 
     let _config = matches.value_of("config").unwrap_or("default.conf");
-    let input_file = matches.value_of("INPUT_FILE").unwrap(); // this is a required parameter
-    match run(&input_file) {
+
+    match run(matches) {
         Ok(lineups) => {
             for lineup in lineups {
                 println!("{}", lineup.to_string())
